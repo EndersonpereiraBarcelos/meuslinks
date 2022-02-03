ea320b579c9aa868a8236fc1f2c344085139ffe0
pbenderson <souza98120@gmail.com>
2022-02-03 12:00:51 -0300

    .

---
+++ src/pages/Home/index.js
@@ -0,0 +1,25 @@
+import { FiLink } from 'react-icons/fi';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="container-home">
+            <div className="logo">
+                <img src="/logo.png" alt="Sujeito Link Logo" />
+                <h1>Sujeito Link</h1>
+                <span>Cole seu Link para encurtar👇</span>
+            </div>
+
+            <div className="area-input">
+                <div>
+                    <FiLink size={24} color="#fff" />
+                    <input placeholder="cole seu link aqui.." />
+                </div>
+
+                <button>Gerar Link</button>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/Links/index.js
@@ -0,0 +1,9 @@
+function Links() {
+    return (
+        <div>
+            <h1>Pagina Meus Links </h1>
+        </div>
+    );
+}
+
+export default Links;
